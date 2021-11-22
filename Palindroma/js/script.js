6e8c3cb6adcbe175ca7cf2e5cba654961949f6a2
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-22 21:23:47 +0100

    ultime considerazioni

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,4 +1,3 @@
-
 let userWord = prompt('Immetti una parola');
 // non serve la parola la possiedo già nell'ordine corretto
 // for(i=0;i<userWord.length; i++){
@@ -8,7 +7,6 @@ let userWord = prompt('Immetti una parola');
 // perciò input funzione parola nella variabile attribuita a prompt e output alla variabile di uscita della funzione
 let reverseWord ='';
 function inverti(userWord){
-    
     for( let i = userWord.length -1 ; i>= 0; i--){
         // IN questo modo elenco solo una lettera alla volta e non tutta la stringa
         // let reverseWord = userWord[i]; 
@@ -24,19 +22,17 @@ let parolaInversa = inverti(userWord);
 // }else{
 //     alert('la parola non è palindroma');
 // }
-
-
 // provo a creare una funzione per la comparazione delle stringhe
 let parola1 = '';
 let parola2 = '';
-let Palindroma;
+let Palindroma = 0;
 function siPalindroma(parola1 , parola2 ){
     if( parola1 === parola2 ){
         alert('la parola è palindroma');
     }else{
         alert('la parola non è palindroma');
     }
-    return Palindroma;
+    // return Palindroma;
 }
 let output1 = siPalindroma( userWord , parolaInversa);
 console.log(output1);
